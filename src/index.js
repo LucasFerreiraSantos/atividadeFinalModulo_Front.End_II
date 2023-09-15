b1ac7640bfb21eaf297a965fa43579f3c919347a
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-09-14 21:59:10 -0300

    feita as função de avançar e voltar a página

--- src/index.js
+++ src/index.js
@@ -1,20 +1,3 @@
-// PEGUEI O TOTAL DE PERSONAGENS
-async function totalCharacters() {
-  const data_api = document.getElementById("data_api");
-
-  await fetch("https://rickandmortyapi.com/api/character")
-    .then((res) => {
-      res.json().then((data) => {
-        const characters = data;
-        data_api.innerHTML = `
-        <p>PERSONAGENS: ${characters.info.count}</p>
-        <p>EPISÓDIOS: ${characters.results[0].episode.length}</p>
-        `;
-      });
-    })
-    .catch((err) => console.error("Não é possível achar a informação", err));
-}
-
 // GERADOR DA LISTA DE CARDS
 async function getCharacters() {
   const characterContainer = document.querySelector(".character_list");
@@ -45,6 +28,38 @@ async function getCharacters() {
       console.error("Não foi possível encontrar a informação", err)
     );
 }
+
+// PEGAR O TOTAL DE PERSONAGENS
+async function totalCharacters() {
+  const data_api = document.getElementById("data_api");
+
+  await fetch("https://rickandmortyapi.com/api/character")
+    .then((res) => {
+      res.json().then((data) => {
+        const characters = data;
+        data_api.innerHTML = `
+        <p>PERSONAGENS: ${characters.info.count}</p>
+        <p>EPISÓDIOS: ${characters.results[0].episode.length}</p>
+        `;
+      });
+    })
+    .catch((err) => console.error("Não é possível achar a informação", err));
+}
+
+// VOLTAR UMA PÁGINA
+const btnPaginatePrevious = document.getElementById("btnPaginatePrevious");
+btnPaginatePrevious.addEventListener("click", (e) => {
+  e.preventDefault();
+  const currentPages = 4;
+  const changePage = window.location(
+    `https://rickandmortyapi.com/api/character/?page=${currentPages - 1}`
+  );
+  console.log(changePage);
+});
+
+// AVANÇAR UMA PÁGINA
+const btnPaginateNext = document.getElementById("btnPaginateNext");
+btnPaginateNext.addEventListener("click", (e) => {});
 
 getCharacters();
 totalCharacters();
